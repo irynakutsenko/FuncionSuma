d34f1879469e01bb84112ea8e528f4f6d03607fd
Iryna Kutsenko <79517296+irynakutsenko@users.noreply.github.com>
2021-10-14 09:20:01 +0200

    FuncionSuma

---
+++ js03.js
@@ -0,0 +1,13 @@
+let num1 = parseInt(prompt("Añade primer numero"));
+console.log(num1);
+
+let num2 = parseInt(prompt("Añade otro numero mas"));
+console.log(num2);
+
+function suma(num1, num2) {
+    let result = num1 + num2;
+    return result;
+
+}
+let resustadoSuma = suma(1, 2);
+console.log(resustadoSuma);
